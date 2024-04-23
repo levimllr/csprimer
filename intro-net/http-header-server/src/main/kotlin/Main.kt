15e3426f1f8566e9a47151e1a3a4e6649c909cf4
Levi Miller <lmiller@squareup.com>
2024-04-23 17:23:37 -0600

    wip

--- intro-net/http-header-server/src/main/kotlin/Main.kt
+++ intro-net/http-header-server/src/main/kotlin/Main.kt
@@ -5,47 +5,80 @@ import java.io.InputStreamReader
 import java.net.ServerSocket
 
 fun main() {
-    val port = 8888;
-    val server = ServerSocket(port)
-    println("Server running locally on port $port")
+  val port = 8888;
+  val server = ServerSocket(port)
+  println("Server running locally on port $port")
+
+  while (true) {
+    val client = server.accept()
+    println("Client connected from ${client.inetAddress.hostAddress}:${client.port}")
+    val writer = client.getOutputStream()
 
     while (true) {
-        val client = server.accept()
-        println("Client connected from ${client.inetAddress.hostAddress}:${client.port}")
-        val writer = client.getOutputStream()
+      // TODO: Keep calling receive? Or just assume entire request
+      val reader =
+        BufferedReader(InputStreamReader(client.getInputStream()))
 
-        while (true) {
-            val reader =
-                BufferedReader(InputStreamReader(client.getInputStream()))
-
-            // TODO: What are the raw incoming bytes when I hit enter in nc?
-            val line = mutableListOf<Int>()
-            var char = reader.read()
-            while (char != -1) {
-                line.add(char)
-                char = reader.read()
-                if (!reader.ready()) {
-                    break
-                }
-            }
-
-            val chars = line.map { it.toChar() }.joinToString("").uppercase()
-            println(chars)
-            writer.write(chars.toByteArray())
-            writer.write("\n".toByteArray())
-
-            if (Regex("(EXIT|QUIT)").matches(chars)) {
-                writer.write("Goodbye!".toByteArray())
-                reader.close()
-                break
-            } else {
-                continue
-            }
+      // TODO: What are the raw incoming bytes when I hit enter in nc?
+      val chars = mutableListOf<Char>()
+      var charInt = reader.read()
+      var headerKey = ""
+      val headers = mutableMapOf<String, String>();
+      while (charInt != -1) {
+        chars.add(charInt.toChar())
+        println(chars[0])
+        if (chars.size >= 5 && chars.subList(chars.size - 5, chars.size - 1).joinToString { "" }
+            .contains("\r\n\r\n")) {
+          println(headers)
+          println(headers)
+          chars.clear()
+          break
+        } else if (chars.size >= 2 && chars.subList(chars.size - 2, chars.size - 1).joinToString { "" }
+            .contains("\r\n")) {
+          val headerValue = chars.joinToString("")
+          headers[headerKey] = headerValue
+          println("$headerKey:$headerValue")
+          headerKey = ""
+          chars.clear()
+        } else if (chars[chars.size - 1] == ':') {
+          headerKey = chars.joinToString("")
+          println("headerKey = $headerKey")
+          chars.clear()
         }
 
-        // TODO: Still need this?
-        client.close()
-        server.close()
-        break
+        if (!reader.ready()) {
+          break
+        } else {
+          charInt = reader.read()
+          continue
+        }
+      }
+
+      /*
+       * TODO
+       * Headers and body separated by \r\n\r\n.
+       * Each header separated by \r\n.
+       * Can you do it in one pass?
+       *
+       * "Be liberal in what you accept, and conservative in what you send." - Jon Postel
+       */
+
+      val serializedHeaders = "{\n"
+      for (header in headers) {
+        serializedHeaders.plus("  \"${header.key}\": \"${header.value}\"\n")
+      }
+      serializedHeaders.plus("}\n")
+      println(serializedHeaders)
+      // Need the follow to conform to HTTP/1.1
+      writer.write("HTTP/1.1 200 OK\r\n\r\n".toByteArray())
+      writer.write(serializedHeaders.toByteArray())
+
+      break
     }
+
+    // TODO: Still need this?
+    client.close()
+    server.close()
+    break
+  }
 }
